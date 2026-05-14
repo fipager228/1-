09fa2ea2c8fb3e194e52cff37380b02231c91762
FiPager <hiroochka301@gmail.com>
2026-05-15 02:21:45 +0300

    Update fgameF.py

--- fgameF.py
+++ fgameF.py
@@ -1,4 +1,6 @@
 
+from prettytable import PrettyTable
+
 
 class Person:
     def __init__(self, name,atk,defe,lvl,hp):
@@ -13,29 +15,43 @@ class Person:
 
     def prov(self):
         if self.hp <= 0:
-            print(f'игрок {self.name} умер ')
+            print(f'игрок {self.name} умер \n')
 
     def attake(self,other):
         if other.hp <= 0:
-            print("противник мёртв, нельзя отоковать. ")
+            print("противник мёртв, нельзя отоковать.\n ")
 
         else:
+            print(f'игрок {self.name} атакует! \n')
             if self.energ >= 2:
                 other.hp -= self.atk
-                print(f'игрок {self.name} нанёс урон {self.atk} игроку {other.name}.')
+                print(f'игрок {self.name} нанёс урон {self.atk} игроку {other.name}.\n')
                 if other.hp <= 0:
                     other.prov()
                 elif other.hp > 0:
-                    print(f'у противника осталось {other.hp} хп.')
+                    print(f'у противника осталось {other.hp} хп.\n')
                 self.energ -= 2
             elif self.energ < 2:
-                print('нет энергии')
+                print('нет энергии\n')
+
+    def invent(self):
+        table = PrettyTable()
+        table.field_names = ["ваш инвентарь :"]
+
+        for i in self.inv:
+            table.add_row([i])
+        print(table)
+
+    def pickup(self,other):
+        self.inv.append(other)
+
+
 
 
 nps = Person('nps_1',100,100,1,100)
 nps_1 = Person('nps_2',100,100,1,100)
-nps.attake(nps_1)
-nps.attake(nps_1)
-nps.attake(nps_1)
+
+nps.pickup('лосось')
+nps.invent()
 
 
